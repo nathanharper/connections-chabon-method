c36d9fcb8bfed4444615e539fc9622411bed2997
Nathan Harper <nathan.sam.harper@gmail.com>
2024-05-05 08:24:45 -0400

    updates

--- src/app/App.js
+++ src/app/App.js
@@ -72,8 +72,8 @@ function LockOverlay({id, lockData, onSubmit, onCancel}) {
             />
           </label>
         </div>
-        <input type="submit" value="lock" />
-        <input type="button" value="cancel" onClick={() => onCancel(id)} />
+        <div><input type="submit" value="lock" /></div>
+        <div><input type="button" value="cancel" onClick={() => onCancel(id)} /></div>
       </form>
     </div>
   );
